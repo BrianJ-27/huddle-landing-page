b146354b45edf62cb699412dfaa9a7b7a334efcd
Brian Johnson <johnson8727@gmail.com>
2022-03-31 18:00:06 -0400

    styling in mobile mode

--- src/App.js
+++ src/App.js
@@ -1,32 +1,44 @@
 import Logo from "./assets/images/logo.svg";
 import HeroImg from "./assets/images/illustration-mockups.svg";
+import { FaFacebookF, FaTwitter, FaInstagram } from "react-icons/fa";
+import React from "react";
 
 function App() {
   return (
-    <div className="App">
-      <header>
-        <img src={Logo} alt="Huddle Logo" />
+    <React.Fragment>
+      <header className="header">
+        <img src={Logo} alt="Huddle Logo" className="img__logo" />
       </header>
       <main>
-        <section>
-          <img src={HeroImg} alt="Mobile and desktop screens" />
+        <section className="hero">
+          <img
+            src={HeroImg}
+            alt="Mobile and desktop screens"
+            className="img__hero"
+          />
         </section>
-        <section>
-          <h1>Build The Community Your Fans Will Love</h1>
-          <p>
+        <section className="community-info">
+          <h1 className="title__primary">
+            Build The Community Your Fans Will Love
+          </h1>
+          <p className="content">
             Huddle re-imagines the way we build communities. You have a voice,
             but so does your audience. Create connections with your users as you
             engage in genuine discussion.
           </p>
-          <a role="button" href="#register">
+          <a role="button" href="#register" className="btn">
             Register
           </a>
         </section>
-        <section className="icons__social"></section>
+        <section className="social">
+          <FaFacebookF className="img__social-icons" />
+          <FaTwitter className="img__social-icons" />
+          <FaInstagram className="img__social-icons" />
+        </section>
       </main>
 
       <footer>
-        <p class="attribution">
+        <p className="attribution">
           Challenge by{" "}
           <a
             href="https://www.frontendmentor.io?ref=challenge"
@@ -38,7 +50,7 @@ function App() {
           . Coded by <a href="#bb">Your Name Here</a>.
         </p>
       </footer>
-    </div>
+    </React.Fragment>
   );
 }
 
