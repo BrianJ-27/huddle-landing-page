248a644a0b70d75954d1beb6faadcac44f2187e0
Brian Johnson <johnson8727@gmail.com>
2022-03-31 08:02:10 -0400

    added design, image and style guide to project

--- src/App.js
+++ src/App.js
@@ -1,5 +1,28 @@
 function App() {
-  return <div className="App"></div>;
+  return (
+    <div className="App">
+      <main>
+        Build The Community Your Fans Will Love Huddle re-imagines the way we
+        build communities. You have a voice, but so does your audience. Create
+        connections with your users as you engage in genuine discussion.
+        Register
+      </main>
+
+      <footer>
+        <p class="attribution">
+          Challenge by{" "}
+          <a
+            href="https://www.frontendmentor.io?ref=challenge"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Frontend Mentor
+          </a>
+          . Coded by <a href="#bb">Your Name Here</a>.
+        </p>
+      </footer>
+    </div>
+  );
 }
 
 export default App;
