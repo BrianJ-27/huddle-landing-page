20a5267cfbcc1baaab385f81065b70489e13bda8
Brian Johnson <johnson8727@gmail.com>
2022-03-31 16:09:07 -0400

    adding all style and variable properties into the project

--- src/App.js
+++ src/App.js
@@ -1,11 +1,28 @@
+import Logo from "./assets/images/logo.svg";
+import HeroImg from "./assets/images/illustration-mockups.svg";
+
 function App() {
   return (
     <div className="App">
+      <header>
+        <img src={Logo} alt="Huddle Logo" />
+      </header>
       <main>
-        Build The Community Your Fans Will Love Huddle re-imagines the way we
-        build communities. You have a voice, but so does your audience. Create
-        connections with your users as you engage in genuine discussion.
-        Register
+        <section>
+          <img src={HeroImg} alt="Mobile and desktop screens" />
+        </section>
+        <section>
+          <h1>Build The Community Your Fans Will Love</h1>
+          <p>
+            Huddle re-imagines the way we build communities. You have a voice,
+            but so does your audience. Create connections with your users as you
+            engage in genuine discussion.
+          </p>
+          <a role="button" href="#register">
+            Register
+          </a>
+        </section>
+        <section className="icons__social"></section>
       </main>
 
       <footer>
